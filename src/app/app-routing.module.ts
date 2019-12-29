38954fb481612f83e04b095f19645cfe159ce6c1
SerhiiShcheh <serhii.shchehelskyi.kn.2017@lpnu.ua>
2019-12-29 22:55:37 +0200

    feature/2 routing added with 2 modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then(mod => mod.HomeModule),
+  },
+  {
+    path: "contacts",
+    loadChildren: () => import("./contacts/contacts.module").then(mod => mod.ContactsModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
