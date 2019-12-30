65bdafa7dd5f8039427097fb0755ee2463e1f5e1
SerhiiShcheh <serhii.shchehelskyi.kn.2017@lpnu.ua>
2019-12-31 01:39:54 +0200

    feature/4 home page mobile version

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { FormComponent } from './form/form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FormComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class HomeModule { }
